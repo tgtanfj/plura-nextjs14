8a0bfb411130e9b1840a0f35a799d58850677942
trungtan <trungtan14@gmail.com>
2024-04-18 13:26:59 +0700

    send invitation

--- src/app/(main)/subaccount/page.tsx
+++ src/app/(main)/subaccount/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 type Props = {}
 
+// kiểm tra nếu không thuộc hoặc chưa có agency nào thì return người dùng đi
+// tìm sub account đầu tiên mà người dùng có access và gửi người dùng đến subaccount
+
 const SubaccountMainPage = (props: Props) => {
   return (
     <div>SubaccountMainPage</div>
