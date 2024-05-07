e8cc14cae2f21a42cdb2a70e6d90df64e14310ca
trungtan <trungtan14@gmail.com>
2024-05-07 14:36:56 +0700

    editor provider

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import React from "react";
 
-type Props = {}
+type Props = {
+  params: {
+    subaccountId: string;
+    funnelId: string;
+    funnelPageId: string;
+  };
+};
 
-const FunnelPageId = (props: Props) => {
+const FunnelPageId = async ({ params }: Props) => {
+  const funnelPageDetails = await db.funnelPage.findFirst({
+    where: {
+      id: params.funnelPageId,
+    },
+  });
+
+  if (!funnelPageDetails) {
+    return redirect(
+      `/subaccount/${params.subaccountId}/funnels/${params.funnelId}`
+    );
+  }
+
   return (
-    <div>FunnelPageId</div>
-  )
-}
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden">
+      <EditorProvider>
 
-export default FunnelPageId
+      </EditorProvider>
+    </div>
+  );
+};
+
+export default FunnelPageId;
